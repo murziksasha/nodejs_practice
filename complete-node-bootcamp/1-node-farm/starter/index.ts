2a94d9731330b0637644efe7ec8192ab4edc7654
Sasha Grigoriev <truedevelop@yahoo.com>
2025-05-27 20:30:18 +0300

    html template cards implemented

--- complete-node-bootcamp/1-node-farm/starter/index.ts
+++ complete-node-bootcamp/1-node-farm/starter/index.ts
@@ -2,20 +2,53 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  return output;
+}
+
     const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-      const dataObj = JSON.parse(data);
+    const dataObj = JSON.parse(data);
+    const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+  // Overview page
   if(pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW');
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+  // Product page
   } else if(pathName === '/product') {
-    res.end('This is the PRODUCT');
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    res.end(tempProduct);
+
+  // API
   } else if(pathName === '/api') {
       res.writeHead(200, {
         'Content-type': 'application/json',
       });
       res.end(data);
+
+  // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
