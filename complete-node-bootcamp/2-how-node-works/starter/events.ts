da8203019860408aed02e2af69ffc7e20e96dd56
Alexander Grygoriev <truedevelop@yahoo.com>
2025-06-04 17:17:13 +0300

    listener practice done

--- complete-node-bootcamp/2-how-node-works/starter/events.ts
+++ complete-node-bootcamp/2-how-node-works/starter/events.ts
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 class MyEmitter extends EventEmitter {
   constructor() {
@@ -14,13 +15,32 @@ class MyEmitter extends EventEmitter {
 
 const myEmitter = new MyEmitter();
 
+myEmitter.on('newSale', (item) => {
+  console.log('A new sale has occurred!', item);
+});
 
-myEmitter.on('newSale', () => {
-  console.log('A new sale has occurred!');
+myEmitter.on('newSale', (item) => {
+  console.log(`Another listener for newSale event! ${item}`);
 });
 
 myEmitter.on('greet', () => {
   console.log('Hello World!');
 });
 
-myEmitter.emit('newSale');
+myEmitter.emit('newSale', 99);
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received!');
+  console.log('Another request received! 💕📞👌❌');
+  res.end('Request received! 💕📞👌❌');
+});
+
+server.on('close', () => {
+  console.log('Server closed!');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Server is listening on port 3000');
+});
