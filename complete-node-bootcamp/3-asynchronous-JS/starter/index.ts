7ca071eba64d7c6bf1c991ce31aff4589930d3ea
Alexander Grygoriev <truedevelop@yahoo.com>
2025-06-10 16:34:46 +0300

    callback learning

---
+++ complete-node-bootcamp/3-asynchronous-JS/starter/index.ts
@@ -0,0 +1,10 @@
+
+const fs = require('fs');
+
+fs.readFile('dog.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log('File content:', data);
+});
