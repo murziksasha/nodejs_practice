e4190eda26cff3f70a6def9466a6cc17716b3662
Sasha Grigoriev <truedevelop@yahoo.com>
2025-06-11 04:31:09 +0300

    callback problem learning

--- complete-node-bootcamp/3-asynchronous-JS/starter/index.ts
+++ complete-node-bootcamp/3-asynchronous-JS/starter/index.ts
@@ -1,5 +1,5 @@
-
 const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile('dog.txt', 'utf8', (err, data) => {
   if (err) {
@@ -7,4 +7,20 @@ fs.readFile('dog.txt', 'utf8', (err, data) => {
     return;
   }
   console.log('File content:', data);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) {
+        console.error('Error fetching image:', err);
+        return;
+      }
+      console.log(res.body);
+      fs.writeFile('dog-img.txt', res.body.message, err => {
+        if (err) {
+          console.error('Error writing file', err);
+          return;
+        }
+        console.log('Rand dog image saved to file');
+      });
+    });
 });
