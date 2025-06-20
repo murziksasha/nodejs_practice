73051d92cbb1bfad6b80a46176a5d04c7d597489
Sasha Grigoriev <truedevelop@yahoo.com>
2025-06-20 08:29:34 +0300

    async await practice

--- complete-node-bootcamp/3-asynchronous-JS/starter/index.ts
+++ complete-node-bootcamp/3-asynchronous-JS/starter/index.ts
@@ -15,7 +15,7 @@ const readFilePromise = (file) => {
 
 const writeFilePromise = (file, data) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(file, data, (err) => {   
+    fs.writeFile(file, data, (err) => {
       if (err) {
         reject(`Could not write file 😢, reason: ${err}`);
       } else {
@@ -23,8 +23,32 @@ const writeFilePromise = (file, data) => {
       }
     });
   });
-}
+};
 
+const getDogPic = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    console.log(res.body.message);
+    await writeFilePromise(`${__dirname}/dog-img.txt`, res.body.message);
+    console.log('Random dog image saved to file successfully!');
+  } catch (err) {
+    throw err;
+  }
+  console.log('2: Ready 🐩');
+};
+console.log('1: Will get dog pics!');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+  });
+
+/*
 readFilePromise(`${__dirname}/dog.txt`).then((data) => { 
   console.log(`Breed: ${data}`);
   return superagent
@@ -40,3 +64,4 @@ readFilePromise(`${__dirname}/dog.txt`).then((data) => {
     });
 });
 });
+*/
