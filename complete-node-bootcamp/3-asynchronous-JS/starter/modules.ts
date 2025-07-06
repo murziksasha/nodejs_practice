14cf4f4f90de68bd580ce2cb18fe32ba8ff97adb
Alexander Grygoriev <truedevelop@yahoo.com>
2025-07-06 23:17:54 +0300

    modules practice

---
+++ complete-node-bootcamp/3-asynchronous-JS/starter/modules.ts
@@ -0,0 +1,4 @@
+const Calc = require('./test-module-1.ts');
+
+const calc = new Calc();
+console.log(calc.add(2, 3));
---
+++ complete-node-bootcamp/3-asynchronous-JS/starter/test-module-1.ts
@@ -0,0 +1,18 @@
+class Calculator {
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  divide(a: number, b: number): number {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+  }
+}
+
+module.exports = Calculator;
