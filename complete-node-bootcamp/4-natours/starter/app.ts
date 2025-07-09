e1594a529d888dcb50046d46999dcecf73612246
Alexander Grygoriev <truedevelop@yahoo.com>
2025-07-10 01:09:10 +0300

    post endpoint added

--- complete-node-bootcamp/4-natours/starter/app.ts
+++ complete-node-bootcamp/4-natours/starter/app.ts
@@ -18,6 +18,30 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'fail',
+          message: 'Error saving the new tour',
+        });
+      }
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
