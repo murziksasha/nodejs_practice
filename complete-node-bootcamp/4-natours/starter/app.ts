622199bf5e41cee7b20fb13ebce686af2339927a
Alexander Grygoriev <truedevelop@yahoo.com>
2025-07-10 08:10:02 +0300

    continue post endpoint develop

--- complete-node-bootcamp/4-natours/starter/app.ts
+++ complete-node-bootcamp/4-natours/starter/app.ts
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
 const tours = JSON.parse(fs.readFileSync(
   path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
   'utf-8'
@@ -19,27 +21,29 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: 'fail',
-          message: 'Error saving the new tour',
-        });
-      }
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  console.log(req.body);
+  res.status(201).send(req.body);
+  // const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     if (err) {
+  //       return res.status(500).json({
+  //         status: 'fail',
+  //         message: 'Error saving the new tour',
+  //       });
+  //     }
+  //     res.status(201).json({
+  //       status: 'success',
+  //       data: {
+  //         tour: newTour,
+  //       },
+  //     });
+  //   }
+  // );
 });
 
 const port = 3000;
