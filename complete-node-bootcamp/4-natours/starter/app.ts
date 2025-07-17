d939124a01f9f0f7b43717aefbec8c982751d607
Alexander Grygoriev <truedevelop@yahoo.com>
2025-07-18 01:06:18 +0300

    some changes for

--- complete-node-bootcamp/4-natours/starter/app.ts
+++ complete-node-bootcamp/4-natours/starter/app.ts
@@ -6,44 +6,45 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
-const tours = JSON.parse(fs.readFileSync(
-  path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
-  'utf-8'
-));
+const tours = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
+    'utf-8'
+  )
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tours
+      tours,
     },
   });
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.status(201).send(req.body);
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   path.join(__dirname, 'dev-data', 'data', 'tours-simple.json'),
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     if (err) {
-  //       return res.status(500).json({
-  //         status: 'fail',
-  //         message: 'Error saving the new tour',
-  //       });
-  //     }
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${path.join(__dirname, 'dev-data', 'data', 'tours-simple.json')}`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'fail',
+          message: 'Error saving the new tour',
+        });
+      }
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
