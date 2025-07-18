a86f931fdf3bb480238a17ce1de93c41c0a711c8
Alexander Grygoriev <truedevelop@yahoo.com>
2025-07-18 03:09:30 +0300

    patch completed

--- complete-node-bootcamp/4-natours/starter/app.ts
+++ complete-node-bootcamp/4-natours/starter/app.ts
@@ -20,7 +20,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});app.get('/api/v1/tours/:id', (req, res) => {
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find(t => t.id === id);
   if (!tour) {
@@ -32,7 +34,7 @@ app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tours
+      tour
     },
   });
 });
@@ -59,6 +61,50 @@ app.post('/api/v1/tours', (req, res) => {
         data: {
           tour: newTour,
         },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if(id > tours.length || id < 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+      res.status(200).json({
+        status: 'success',
+        data: {
+          tour: '<Updated tour data here>',
+        },
+      });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tourIndex = tours.findIndex(t => t.id === id);
+  if (tourIndex === -1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+  tours.splice(tourIndex, 1);
+  fs.writeFile(
+    `${path.join(__dirname, 'dev-data', 'data', 'tours-simple.json')}`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'fail',
+          message: 'Error deleting the tour',
+        });
+      }
+      res.status(204).json({
+        status: 'success',
+        data: null,
       });
     }
   );
