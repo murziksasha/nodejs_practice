673055b4d8f0f40c28ef1cd466690fe0443d63c5
Sasha Grigoriev <truedevelop@yahoo.com>
2025-09-10 08:14:24 +0300

    db connected via mongoose

--- complete-node-bootcamp/4-natours/starter/server.ts
+++ complete-node-bootcamp/4-natours/starter/server.ts
@@ -1,6 +1,22 @@
 
-
+import * as mongoose from "mongoose";
+import * as dotenv from "dotenv";
 import app from "./app";
+dotenv.config({ path: "./config.env" });
+// Connect to the database
+
+const DB = process.env.DATABASE!.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD!
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    // useUnifiedTopology: true,  // These options are no longer supported in Mongoose 6 and above
+  })
+  .then(() => console.log("DB connection successful!  "));
 
 // Start the server
 
