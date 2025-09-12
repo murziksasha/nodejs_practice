8d379d90c6753b4293251432d50a55635e99bdf1
Alexander Grygoriev <truedevelop@yahoo.com>
2025-09-12 08:49:50 +0300

    testTour creature added by mongoose in app

--- complete-node-bootcamp/4-natours/starter/server.ts
+++ complete-node-bootcamp/4-natours/starter/server.ts
@@ -19,6 +19,42 @@ mongoose
   })
   .then(() => console.log("DB connection successful!  "));
 
+  const tourSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: [true, "Tour name must be unique"],
+      maxlength: [40, "A tour name must have less or equal then 40 characters"],
+      minlength: [10, "A tour name must have more or equal then 10 characters"],
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+    },
+  });
+
+    const Tour = mongoose.model("Tour", tourSchema);
+    const testTour = new Tour({
+      name: "The Park Camper",
+      rating: 4.7,
+      price: 997,
+    });
+    testTour
+      .save()
+      .then((doc) => {
+        console.log(doc);
+      })
+      .catch((err) => {
+        console.log("Error 💥💥💥: ", err);
+      });
+
 
 
 // Start the server
