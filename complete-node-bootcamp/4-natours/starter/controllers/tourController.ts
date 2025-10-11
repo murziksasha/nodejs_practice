4b34e0e38231e9d69f0a5c893a15afd9608b6816
Sasha Grigoriev <truedevelop@yahoo.com>
2025-10-11 08:27:57 +0300

    feat: advanced filtering during API queries

--- complete-node-bootcamp/4-natours/starter/controllers/tourController.ts
+++ complete-node-bootcamp/4-natours/starter/controllers/tourController.ts
@@ -14,11 +14,21 @@ const checkBody = (req, res, next) => {
 
 const getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
+    //{ 'duration[gte]': '5', difficulty: 'easy' }
+    //{ 'duration[$gte]': '5', difficulty: 'easy' } - after replace
+
     //Build the query
-    const queryobject = { ...req.query };
+    //1) Filtering
+    const queryObject = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryobject[el]);
-    const query = Tour.find(queryobject);
+    excludedFields.forEach((el) => delete queryObject[el]);
+    
+    //2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObject);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    
+    const query = Tour.find(JSON.parse(queryStr));
 
     //Execute Query
     const tours = await query;
